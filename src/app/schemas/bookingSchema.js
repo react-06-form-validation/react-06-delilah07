a58b152f80d8c27e17b77d35ba5cdf9370051146
Iryna Abrosimova <abrosimovairyna@gmail.com>
2026-03-24 15:29:21 +0100

    resolve test errors

--- src/app/schemas/bookingSchema.js
+++ src/app/schemas/bookingSchema.js
@@ -5,10 +5,12 @@ export const createBookingSchema = (validSlots) =>
   z.object({
     bookerName: z.string().min(2, "Booker name must be at least 2 characters long").or(z.literal("")),
     bookerEmail: z.email("Invalid email address").optional().or(z.literal("")),
-    eventName: z.string().min(2, "Event name is required"),
+    eventName: z.string("Event name is required").min(2, "Event name must be at least 2 characters long"),
     eventDate: z.date().min(new Date(), {message: "Event date must be in the future"}),
-    numberOfGuests: z.coerce.number("Number of Guests must be less than or equal to 10").int().min(1).max(10),
-    timeSlot: z.string(validSlots),
+    numberOfGuests: z.coerce.number().int().min(1, "Number of Guests must be less than or equal to 10").max(10, "Number of Guests must be less than or equal to 10"),
+    timeSlot: z.string().refine((val) => validSlots.includes(val), {
+      message: "Selected time slot is unavailable",
+    }),
     eventLink: z.url("Invalid URL. Please enter a valid event link"),
   });
 
